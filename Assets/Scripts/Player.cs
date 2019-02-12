ddcaeae7fb1d352846650ce12aad0205e73dacc1
Pedro Sanches <pedro_sanches_96@hotmail.com>
2019-02-12 22:59:27 +0000

    Codificação do movimento do Player (Esquerda e Direita)
    * Codificação do movimento do Player
      - Utilizando A ou <- ;
      - Utilizando D ou -> ;
    
    Parte de codificação do Salto (Jump)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,7 +10,13 @@ public class Player : MonoBehaviour {
     #region Declaração de Variáveis
 
     //Configurações
-    [SerializeField] float speed = 5f; //Variável que define a velocidade de movimentação da personagem
+    float speed = 1f; //Variável que define a velocidade de movimentação da personagem
+    bool facingRight = true; //Variável que guarda a direção para onde o Player está virado
+    public float fallMultiplier = 2.5f; //Multiplicador de queda ao saltar
+    public float lowJumpMultiplier = 2f; //Multiplicador de salto 
+    [Range(1, 10)]
+    public float jumpVelocity;
+
 
     //Componentes em cache
     private Rigidbody2D player;
@@ -30,15 +36,13 @@ public class Player : MonoBehaviour {
     }
 
     /// <summary>
-    /// Método Update que é invocado a cada frame
+    /// Método FixedUpdate que é invocado a cada instante
     /// </summary>
-    void Update()
+    void FixedUpdate()
     {
         //Chamada do método que permite a movimentação do Player
         PlayerMovement();
-
-        //Chamada do método que troca a direção do Sprite do Player
-        FlipSprite();
+        
     }
 
     /// <summary>
@@ -65,15 +69,18 @@ public class Player : MonoBehaviour {
                 //Se a personagem se encontrava em modo Idle
                 if (animator.GetBool("Idle") == true)
                 {
-                    //Coloca a falso o modo de Idle
+                    //Coloca a falso o modo de Idle e de Jumping
                     animator.SetBool("Idle", false);
+                    animator.SetBool("Jumping", false);
+
+                    //Definição da Animação
+                    animator.SetBool("Running", true);
 
                     //Definição da deslocação do Player
                     Vector2 playerVelocity = new Vector2(hAxis * speed, player.velocity.y);
                     player.velocity = playerVelocity;
 
-                    //Definição da Animação
-                    animator.SetBool("Running", true);
+                    
                 }
 
             }
@@ -86,15 +93,17 @@ public class Player : MonoBehaviour {
                 //Se a personagem se encontrava em modo Idle
                 if (animator.GetBool("Idle") == true)
                 {
-                    //Coloca a falso o modo de Idle
+                    //Coloca a falso o modo de Idle e de Jumping
                     animator.SetBool("Idle", false);
+                    animator.SetBool("Jumping", false);
+
+                    //Definição da Animação
+                    animator.SetBool("Running", true);
 
                     //Definição da deslocação do Player
                     Vector2 playerVelocity = new Vector2(hAxis * speed, player.velocity.y);
                     player.velocity = playerVelocity;
-
-                    //Definição da Animação
-                    animator.SetBool("Running", true);
+                    
                 }
             }
             if (dPadY != 0)
@@ -123,12 +132,24 @@ public class Player : MonoBehaviour {
         else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             //Inserir animação de andar com o Player direcionado para a esquerda 
-            //Definição da deslocação do Player
-            transform.Translate(-speed * Time.deltaTime, 0, 0);
+
+            //Valor da input 
+            float hAxis = Input.GetAxis("Horizontal");
+
+            //Condição que verifica se é necessário realizar a troca de direção do Player
+            if (hAxis < 0 && facingRight)
+            {
+                FlipSprite();
+            }
 
             //Definição da Animação
+            animator.SetBool("Jumping", false);
+            animator.SetBool("Idle", false);
             animator.SetBool("Running", true);
-
+            
+            //Definição da deslocação do Player
+            player.velocity = new Vector2(hAxis * speed, player.velocity.y);
+            
             //No caso do personagem estar a andar para a esquerda e o jogador premir a tecla para baixo então o Player irá deslizar nessa direção
             if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
             {
@@ -144,11 +165,22 @@ public class Player : MonoBehaviour {
         //Movimento para a direita do Player
         else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            //Inserir animação de andar com o Player direcionado para a direita 
-            transform.Translate(speed * Time.deltaTime, 0, 0);
+            //Valor da input 
+            float hAxis = Input.GetAxis("Horizontal");
+
+            //Condição que verifica se é necessário realizar a troca de direção do Player
+            if (hAxis > 0 && !facingRight)
+            {
+                FlipSprite();
+            }
 
             //Definição da Animação
+            animator.SetBool("Jumping", false);
+            animator.SetBool("Idle", false);
             animator.SetBool("Running", true);
+
+            //Definição da deslocação do Player
+            player.velocity = new Vector2(hAxis * speed, player.velocity.y);
             
             //No caso do personagem estar a andar para a direita e o jogador premir a tecla para baixo então o Player irá deslizar nessa direção
             if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
@@ -159,12 +191,18 @@ public class Player : MonoBehaviour {
             else if (Input.GetKey(KeyCode.Space))
             {
                 //Inserir animação para saltar o Player se encontra em movimento para a direita
+                
             }
         }
         
         //No caso de IDLE, ativar respetiva animação IDLE que exibe a última direção para a qual o Player estava direcionado
         else
         {
+            //É suposto o Player passar ao estado de IDLE neste 'else', como tal é necessário cancelar qualquer outra animação
+            animator.SetBool("Running", false);
+
+            animator.SetBool("Jumping", false);
+
             //Inserir animação para IDLE com o Player direcionado para a esquerda ou direita consoante a sua última posição
             animator.SetBool("Idle", true);
 
@@ -177,6 +215,20 @@ public class Player : MonoBehaviour {
             else if (Input.GetKey(KeyCode.Space))
             {
                 //Inserir animação para saltar enquanto IDLE com o Player direcionado para a esquerda ou direita consoante a sua última posição
+
+                //Definição do salto do Player
+                player.velocity = Vector2.up * jumpVelocity;
+
+                //Condição que verifica se o Player encontra-se em queda
+                if (player.velocity.y < 0)
+                {
+                    player.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime; // https://www.youtube.com/watch?v=7KiK0Aqtmzc
+                }
+
+                //Definição da Animação
+                animator.SetBool("Idle", false);
+                animator.SetBool("Jumping", true);
+                
             }
 
         }
@@ -190,11 +242,10 @@ public class Player : MonoBehaviour {
     /// </summary>
     void FlipSprite()
     {
-        //Condição que verifica se o Player está a realizar algum movimento na horizontal
-        if (Mathf.Abs(player.velocity.x) > Mathf.Epsilon)
-        {
-            transform.localScale = new Vector2(Mathf.Sign(player.velocity.x), 1f);
-        }
+        facingRight = !facingRight; //Altera o estado atual do Player
+        Vector3 theScale = transform.localScale; //Guarda o valor em 3D escala do player
+        theScale.x *= -1; //Altera a escala do player em X, invertendo-a
+        transform.localScale = theScale; //Guarda a nova escala do Player
     }
 
     #endregion
